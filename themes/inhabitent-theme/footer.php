3a373d288ca429a91c9c91e7a013838bbe4f7050
DanielKell <danielkell.ubc@gmail.com>
2017-08-11 16:12:57 -0700

    Starter theme files

---
+++ themes/inhabitent-theme/footer.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="container">
+					<div class="contact-info">
+				        <p> info@inhabitent.com </p>
+						<p> 778-456-7891 </p>
+						<p class="footer-logos">
+                            <span>
+                                <a href="#"><i class="fa fa-facebook-square icon" aria-hidden="true"></i></a>
+                            </span>
+                            <span>
+                                <a href="#"><i class="fa fa-twitter-square icon" aria-hidden="true"></i></a>
+                            </span>
+                            <span>
+                                <a href="#"><i class="fa fa-google-plus-square icon" aria-hidden="true"></i></a>
+                            </span>
+                        </p>
+					</div>
+
+					<div class="business-hours">
+						<p><span class="business-hours-days">Monday-Friday:</span> 9am to 5pm </p>
+						<p><span class="business-hours-days">Saturday:</span> 10am to 2pm </p>
+						<p><span class="business-hours-days">Sunday:</span> Closed </p>
+					</div>
+
+					<div class="footer-logo">
+					LOGO HERE
+					</div>
+				</div>
+				
+					<div class="copyright">
+						Copyright 2017 Inhabitent
+					</div>
+
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
