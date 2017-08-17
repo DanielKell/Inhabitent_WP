812c7f30e9f0463cb58fb271cdaddcf6f337b00a
DanielKell <danielkell.ubc@gmail.com>
2017-08-16 22:23:18 -0700

    Started styling Shop Page

--- themes/inhabitent-theme/archive-product.php
+++ themes/inhabitent-theme/archive-product.php
@@ -1,42 +1,53 @@
 <?php
 /**
- * The template for displaying archive pages.
+ * Archive Product pages (for shop)
  *
  * @package RED_Starter_Theme
  */
-
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+		<?php query_posts( array( 'post_type' =>'product', 'orderby' => 'date', 'order' => 'ASC' ) ); ?>
+		<?php
+			$args = array( 'post_type' => 'product-type');
+			$product_types = get_terms( $args ); // returns an array of posts
+		?>
 		<?php if ( have_posts() ) : ?>
-
 			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_title( '<h1 class="page-title text-center">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
+				<div class="flex-container-no-wrap">
+                    <ul class="product-types">
+				<?php foreach ( $product_types as $product_type ) : setup_postdata( $product_type ); ?>
+					<li class="product-type-name">
+						<a class="text-uppercase" href="<?php echo home_url() ?>/product-type/<?php echo $product_type->slug ?>"><?php echo $product_type->name ?></a>
+					</li>
+				<?php endforeach; wp_reset_postdata(); ?>
+                    </ul>
+				</div>
 			</header><!-- .page-header -->
-
-			<?php /* Start the Loop */ ?>
+			<section class="flex-container">
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
-
+        <div class="flex-product-item">
+          <?php if ( has_post_thumbnail() ) : ?>
+					<a href=<?php echo get_post_permalink() ?>><div class="product-thumbnail"><?php the_post_thumbnail( 'large' ); ?></div></a>
+					<?php endif; ?>
+					<div class="product-title-section">
+						<h2 class="entry-title">
+							<span><?php the_title(); ?></span> 
+							<span>.....................................</span>
+							<span><?php echo CFS()->get( 'price' ); ?></span>
+						</h2>
+					</div>
+					
+				</div>
 			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
+			</section>
 		<?php endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php get_footer(); ?>
